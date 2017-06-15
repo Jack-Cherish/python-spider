35c9d24b60899ff5d6396b78242cf087e88d084e
Jack-Cherish <c411184003@gmail.com>
2017-06-15 21:12:37 +0800

    更新：暂只支持PC和手机在线观看VIP视频

--- video_downloader/video_downloader.py
+++ video_downloader/video_downloader.py
@@ -11,7 +11,8 @@ import re
 import json
 import os
 import types
-
+import requests
+import time
 
 
 """
@@ -76,9 +77,9 @@ class APP:
 		label2 = tk.Label(frame_2, text = " ")
 		play = tk.Button(frame_2, text = "播放", font = ('楷体',12), fg = 'Purple', width = 2, height = 1, command = self.video_play)
 		label3 = tk.Label(frame_2, text = " ")
-		download = tk.Button(frame_2, text = "下载", font = ('楷体',12), fg = 'Purple', width = 2, height = 1, command = self.download_wmxz)
+		# download = tk.Button(frame_2, text = "下载", font = ('楷体',12), fg = 'Purple', width = 2, height = 1, command = self.download_wmxz)
 		QR_Code = tk.Button(frame_3, text = "手机观看", font = ('楷体',12), fg = 'Purple', width = 10, height = 2, command = self.QR_Code)
-		label_explain = tk.Label(frame_3, fg = 'red', font = ('楷体',12), text = '\n注意：支持大部分主流视频网站的视频播放和视频下载！\n此软件仅用于交流学习，请勿用于任何商业用途！')
+		label_explain = tk.Label(frame_3, fg = 'red', font = ('楷体',12), text = '\n注意：支持大部分主流视频网站的视频播放！\n此软件仅用于交流学习，请勿用于任何商业用途！')
 		label_warning = tk.Label(frame_3, fg = 'blue', font = ('楷体',12),text = '\n建议：将Chrome内核浏览器设置为默认浏览器\n作者:Jack_Cui')
 
 
@@ -95,7 +96,7 @@ class APP:
 		label2.grid(row = 0, column = 2)
 		play.grid(row = 0, column = 3,ipadx = 10, ipady = 10)
 		label3.grid(row = 0, column = 4)
-		download.grid(row = 0, column = 5,ipadx = 10, ipady = 10)
+		# download.grid(row = 0, column = 5,ipadx = 10, ipady = 10)
 		QR_Code.grid(row = 0, column = 0)
 		label_explain.grid(row = 1, column = 0)
 		label_warning.grid(row = 2, column = 0)
@@ -153,37 +154,37 @@ class APP:
 
 				#获取time、key、url
 				get_url = 'http://www.vipjiexi.com/x2/tong.php?url=%s' % ip 
-				get_url_head = {
-					'User-Agent':'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19',
-					'Referer':'http://www.vipjiexi.com/',
-				}
-				get_url_req = request.Request(url = get_url, headers = get_url_head)
-				get_url_response = request.urlopen(get_url_req)
-				get_url_html = get_url_response.read().decode('utf-8')
-				bf = BeautifulSoup(get_url_html, 'lxml')
-				a = str(bf.find_all('script'))
-				pattern = re.compile('"api.php", {"time":"(\d+)", "key": "(.+)", "url": "(.+)","type"', re.IGNORECASE)
-				string = pattern.findall(a)
-				now_time = string[0][0]
-				now_key = string[0][1]
-				now_url = string[0][2] 
+				# get_url_head = {
+				# 	'User-Agent':'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19',
+				# 	'Referer':'http://www.vipjiexi.com/',
+				# }
+				# get_url_req = request.Request(url = get_url, headers = get_url_head)
+				# get_url_response = request.urlopen(get_url_req)
+				# get_url_html = get_url_response.read().decode('utf-8')
+				# bf = BeautifulSoup(get_url_html, 'lxml')
+				# a = str(bf.find_all('script'))
+				# pattern = re.compile('"api.php", {"time":"(\d+)", "key": "(.+)", "url": "(.+)","type"', re.IGNORECASE)
+				# string = pattern.findall(a)
+				# now_time = string[0][0]
+				# now_key = string[0][1]
+				# now_url = string[0][2] 
 
-				#请求播放,获取Success = 1
-				get_movie_url = 'http://www.vipjiexi.com/x2/api.php'
-				get_movie_data = {
-					'key':'%s' % now_key,
-					'time':'%s' % now_time,
-					'type':'',
-					'url':'%s' % now_url
-				}
-				get_movie_head = {
-					'User-Agent':'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19',
-					'Referer':'http://www.vipjiexi.com/x2/tong.php?',
-					'url':'%s' % ip,
-				}
-				get_movie_req = request.Request(url = get_movie_url, headers = get_movie_head)
-				get_movie_data = parse.urlencode(get_movie_data).encode('utf-8')
-				get_movie_response = request.urlopen(get_movie_req, get_movie_data)
+				# #请求播放,获取Success = 1
+				# get_movie_url = 'http://www.vipjiexi.com/x2/api.php'
+				# get_movie_data = {
+				# 	'key':'%s' % now_key,
+				# 	'time':'%s' % now_time,
+				# 	'type':'',
+				# 	'url':'%s' % now_url
+				# }
+				# get_movie_head = {
+				# 	'User-Agent':'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19',
+				# 	'Referer':'http://www.vipjiexi.com/x2/tong.php?',
+				# 	'url':'%s' % ip,
+				# }
+				# get_movie_req = request.Request(url = get_movie_url, headers = get_movie_head)
+				# get_movie_data = parse.urlencode(get_movie_data).encode('utf-8')
+				# get_movie_response = request.urlopen(get_movie_req, get_movie_data)
 				#请求之后立刻打开
 				webbrowser.open(get_url)
 
@@ -191,7 +192,7 @@ class APP:
 			msgbox.showerror(title='错误',message='视频链接地址无效，请重新输入！')
 
 	"""
-	函数说明:视频下载，通过无名小站抓包
+	函数说明:视频下载，通过无名小站抓包(已经无法使用)
 
 	Parameters:
 		self
@@ -200,14 +201,13 @@ class APP:
 		无
 
 	Modify:
-		2017-05-09
+		2017-06-15
 	"""
 	def download_wmxz(self):	
 		if re.match(r'^https?:/{2}\w.+$', self.url.get()):
 			#视频链接获取
 			ip = self.url.get()
 			#视频链接加密
-			# ip = ip.replace('://','%3A%2F%2F').replace('/', '%2F').replace('?fc=', '%3D').replace('=', '%3D').replace('#', '%23')
 			ip = parse.quote_plus(ip)
 
 			#获取保存视频的url
@@ -257,36 +257,10 @@ class APP:
 			#视频链接获取
 			ip = self.url.get()
 			#视频链接加密
-			# ip = ip.replace('://','%3A%2F%2F').replace('/', '%2F').replace('?fc=', '%3D').replace('=', '%3D').replace('#', '%23')
 			ip = parse.quote_plus(ip)
 
-			#获取保存视频的url
-			get_url = 'http://www.sfsft.com/index.php?url=%s' % ip 
-			head = {
-				'User-Agent':'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19',
-				'Referer':'http://www.sfsft.com/index.php?url=%s' % ip
-			}
-			get_url_req = request.Request(url = get_url, headers = head)
-			get_url_response = request.urlopen(get_url_req)
-			get_url_html = get_url_response.read().decode('utf-8')
-			bf = BeautifulSoup(get_url_html, 'lxml')
-			a = str(bf.find_all('script'))
-			pattern = re.compile("url : '(.+)',", re.IGNORECASE)
-			url = pattern.findall(a)[0]
-
-			#获取视频地址
-			get_movie_url = 'http://www.sfsft.com/api.php'
-			get_movie_data = {
-				'up':'0',
-				'url':'%s' % url,
-			}
-			get_movie_req = request.Request(url = get_movie_url, headers = head)
-			get_movie_data = parse.urlencode(get_movie_data).encode('utf-8')
-			get_movie_response = request.urlopen(get_movie_req, get_movie_data)
-			get_movie_html = get_movie_response.read().decode('utf-8')
-			get_movie_data = json.loads(get_movie_html)
-
-			words = get_movie_data['url']
+			url = 'http://www.wmxz.wang/video.php?url=%s' % ip
+			words = url
 			images_pwd = os.getcwd() + '\Images\\'
 			png_path = images_pwd + 'bg.png'
 			qr_name = 'qrcode.png'
@@ -296,7 +270,7 @@ class APP:
 
 			top = tk.Toplevel(self.root)
 			img = tk.PhotoImage(file = qr_path)
-			text_label = tk.Label(top, fg = 'red', font = ('楷体',15), text = "手机浏览器扫描二维码，在线观看或下载视频！")
+			text_label = tk.Label(top, fg = 'red', font = ('楷体',15), text = "手机浏览器扫描二维码，在线观看视频！")
 			img_label = tk.Label(top, image = img)
 			text_label.pack()
 			img_label.pack()
@@ -344,3 +318,7 @@ class APP:
 if __name__ == '__main__':
 	app = APP()			#实例化APP对象
 	app.loop()			#loop等待用户事件
+
+
+
+
