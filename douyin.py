7f1afba67b4f17fa51e58afb907bc55fc81f8327
Jack-Cherish <c411184003@gmail.com>
2017-11-19 18:07:43 +0800

    抖音APP视频下载

--- douyin.py
+++ douyin.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 from bs4 import BeautifulSoup
 from contextlib import closing
-import requests, json, time, re, os, sys, types
+import requests, json, time, re, os, sys, time
 
 class DouYin(object):
 	def __init__(self):
@@ -9,13 +9,9 @@ class DouYin(object):
 		抖音App视频下载
 		"""
 		#SSL认证
-		try:
-			import urllib3.contrib.pyopenssl
-			urllib3.contrib.pyopenssl.inject_into_urllib3()
-		except ImportError:
-			pass
+		pass
 
-	def get_video_urls(self, nickname):
+	def get_video_urls(self, user_id):
 		"""
 		获得视频播放地址
 		Parameters:
@@ -27,26 +23,26 @@ class DouYin(object):
 		"""
 		video_names = []
 		video_urls = []
-		search_url = 'http://api.amemv.com/aweme/v1/discover/search/?cursor=0&keyword=%s&count=10&type=1&retry_type=no_retry&iid=15777917428&device_id=34692364858&ac=wifi&channel=xiaomi&aid=1128&app_name=aweme&version_code=157&version_name=1.5.7&device_platform=android&ssmix=a&device_type=MI+5&device_brand=Xiaomi&os_api=24&os_version=7.0&uuid=861945034132187&openudid=dc451556fc0eeadb&manifest_version_code=157&resolution=1080*1920&dpi=480&update_version_code=1572&ts=%d&app_type=normal' % (nickname, int(time.time()))
-		req = requests.get(url = search_url)
+
+		search_url = 'https://api.amemv.com/aweme/v1/discover/search/?cursor=0&keyword=%s&count=10&type=1&retry_type=no_retry&iid=17900846586&device_id=34692364855&ac=wifi&channel=xiaomi&aid=1128&app_name=aweme&version_code=162&version_name=1.6.2&device_platform=android&ssmix=a&device_type=MI+5&device_brand=Xiaomi&os_api=24&os_version=7.0&uuid=861945034132187&openudid=dc451556fc0eeadb&manifest_version_code=162&resolution=1080*1920&dpi=480&update_version_code=1622' % (user_id)
+		req = requests.get(url = search_url, verify = False)
 		html = json.loads(req.text)
-		for each in html['user_list']:
-			if each['user_info']['nickname'] == nickname:
-				aweme_count = each['user_info']['aweme_count']
-				user_id = each['user_info']['uid']
-				# print('粉丝数:',each['user_info']['follower_count'])
-		user_url = 'http://api.amemv.com/aweme/v1/aweme/post/?user_id=%s&max_cursor=0&count=%s&retry_type=no_retry&iid=15777917428&device_id=34692364858&ac=wifi&channel=xiaomi&aid=1128&app_name=aweme&version_code=157&version_name=1.5.7&device_platform=android&ssmix=a&device_type=MI+5&device_brand=Xiaomi&os_api=24&os_version=7.0&uuid=861945034132187&openudid=dc451556fc0eeadb&manifest_version_code=157&resolution=1080*1920&dpi=480&update_version_code=1572&ts=%d&app_type=normal' % (user_id, aweme_count, int(time.time()))
-		req = requests.get(url = user_url)
+		aweme_count = html['user_list'][0]['user_info']['aweme_count']
+		user_id = html['user_list'][0]['user_info']['uid']
+		nickname = html['user_list'][0]['user_info']['nickname']
+		user_url = 'https://www.douyin.com/aweme/v1/aweme/post/?user_id=%s&max_cursor=0&count=%s' % (user_id, aweme_count)
+		req = requests.get(url = user_url, verify = False)
 		html = json.loads(req.text)
+		i = 1
 		for each in html['aweme_list']:
 			share_desc = each['share_info']['share_desc']
 			if '抖音-原创音乐短视频社区' == share_desc:
-				print(each)
-				video_names.append(each['cha_list'][0]['cha_name'] + '.mp4')
+				video_names.append(str(i) + '.mp4')
+				i += 1
 			else:
 				video_names.append(share_desc + '.mp4')
 			video_urls.append(each['share_info']['share_url'])
-		return video_names, video_urls, aweme_count
+		return video_names, video_urls, nickname
 
 	def get_download_url(self, video_url):
 		"""
@@ -56,7 +52,7 @@ class DouYin(object):
 		Returns:
 			download_url: 视频下载地址
 		"""
-		req = requests.get(url = video_url)
+		req = requests.get(url = video_url, verify = False)
 		bf = BeautifulSoup(req.text, 'lxml')
 		script = bf.find_all('script')[-1]
 		video_url_js = re.findall('var data = \[(.+)\];', str(script))[0]
@@ -73,11 +69,12 @@ class DouYin(object):
 			None
 		"""
 		size = 0
-		with closing(requests.get(video_url, stream=True)) as response:
+		with closing(requests.get(video_url, stream=True, verify = False)) as response:
 			chunk_size = 1024
 			content_size = int(response.headers['content-length']) 
 			if response.status_code == 200:
-				print('  [文件大小]:%0.2f MB' % (content_size / chunk_size / 1024))
+				sys.stdout.write('  [文件大小]:%0.2f MB\n' % (content_size / chunk_size / 1024))
+
 				with open(video_name, "wb") as file:  
 					for data in response.iter_content(chunk_size = chunk_size):
 						file.write(data)
@@ -86,9 +83,10 @@ class DouYin(object):
 
 					sys.stdout.write('    [下载进度]:%.2f%%' % float(size / content_size * 100))
 					sys.stdout.flush()
+		time.sleep(1)
 
 
-	def run(self, nickname):
+	def run(self):
 		"""
 		运行函数
 		Parameters:
@@ -97,14 +95,21 @@ class DouYin(object):
 			None
 		"""
 		self.hello()
-		video_names, video_urls, aweme_count = self.get_video_urls(nickname)
+		user_id = input('请输入ID(例如13978338):')
+		video_names, video_urls, nickname = self.get_video_urls(user_id)
 		if nickname not in os.listdir():
 			os.mkdir(nickname)
-		print('视频下载中:\n')
-		for num in range(aweme_count):
-			print('  ' + video_names[num])
+		sys.stdout.write('视频下载中:\n')
+		for num in range(len(video_urls)):
+			print('  %s\n' % video_urls[num])
 			video_url = self.get_download_url(video_urls[num])
-			self.video_downloader(video_url, os.path.join(nickname.strip(), video_names[num]))
+			if '\\' in video_names[num]:
+				video_name = video_names[num].replace('\\', '')
+			elif '/' in video_names[num]:
+				video_name = video_names[num].replace('/', '')
+			else:
+				video_name = video_names[num]
+			self.video_downloader(video_url, os.path.join(nickname, video_name))
 			print('')
 
 	def hello(self):
@@ -119,7 +124,7 @@ class DouYin(object):
 		print('\t\t\t\t抖音App视频下载小助手')
 		print('*' * 100)
 
+		
 if __name__ == '__main__':
 	douyin = DouYin()
-	nickname = r'徐浪浪and芒果琳'
-	douyin.run(nickname)
+	douyin.run()
