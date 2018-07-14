c7525c383ae151ca40ac0d44fd3859d279b5b2ea
steven7851 <steven7851@msn.com>
2018-07-14 13:37:27 +0800

    typo

--- douyin_pro_2.py
+++ douyin_pro_2.py
@@ -57,7 +57,7 @@ class DouYin(object):
 		except (OSError, IOError) as err:
 			print('请先安装 node.js: https://nodejs.org/')
 			sys.exit()
-		sign = singer.readlines()[0]
+		sign = process.communicate()[0].decode()
 		user_url = 'https://www.amemv.com/aweme/v1/aweme/post/?user_id=%s&max_cursor=0&count=%s&aid=1128&_signature=%s&dytk=%s' % (uid, aweme_count, sign, dytk)
 		req = requests.get(user_url, headers=self.headers)
 		html = json.loads(req.text)
