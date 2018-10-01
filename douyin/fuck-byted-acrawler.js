023d6fd078662940d2cf92ead8f67f6be01c8870
steven7851 <steven7851@msn.com>
2018-10-01 10:19:00 +0800

    Update fuck-byted-acrawler.js

--- douyin/fuck-byted-acrawler.js
+++ douyin/fuck-byted-acrawler.js
@@ -3,9 +3,9 @@ function generateSignature(userId) {
     this.navigator = {
         userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"
     }
-    let e = {}
+    var e = {}
 
-    let r = (function () {
+    var r = (function () {
         function e(e, a, r) {
             return (b[e] || (b[e] = t("x,y", "return x " + e + " y")))(r, a)
         }
