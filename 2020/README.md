bd0e8d4155936d5abb5717d11a8c57808c5b376c
Jack-Cherish <c411184003@gmail.com>
2020-04-20 22:43:51 +0800

    2020

---
+++ 2020/README.md
@@ -0,0 +1,15 @@
+# Python Spider 2020
+
+由于这个项目时间太长了，陆陆续续，很多实战示例也早已失效。
+
+网络爬虫，是一门比较通用的基础技术，各个领域都会有所涉及，比如我做视觉算法的，也需要用到网络爬虫，例如调用 API 接口清洗数据等，这本质也都是一个小的爬虫程序。
+
+为了提供各位更好的学习示例，我决定重写这一系列教程，对一些失效的示例，重新找例子，并查缺补漏，完善这一些列教程。
+
+2020年，最新版的 Python3 网络爬虫实战系列教程，我会首发在公众号和个人网站上。
+
+* 公众号：[JackCui-AI](https://mp.weixin.qq.com/s/OCWwRVDFNslIuKyiCVUoTA "JackCui-AI")<br>
+
+<div align="center"><img border="0" src="images/gzh-1.jpg" alt="Coder" title="gongzhonghao" with="200" height="200"></div>
+
+* [个人网站](http://cuijiahua.com "个人网站")
---
+++ 2020/xbqg/xbqg_spider.py
@@ -0,0 +1,33 @@
+import requests
+import time
+from tqdm import tqdm
+from bs4 import BeautifulSoup
+
+def get_content(target):
+    req = requests.get(url = target)
+    req.encoding = 'utf-8'
+    html = req.text
+    bf = BeautifulSoup(html, 'lxml')
+    texts = bf.find('div', id='content')
+    content = texts.text.strip().split('\xa0'*4)
+    return content
+
+if __name__ == '__main__':
+    server = 'https://www.xsbiquge.com'
+    book_name = '诡秘之主.txt'
+    target = 'https://www.xsbiquge.com/15_15338/'
+    req = requests.get(url = target)
+    req.encoding = 'utf-8'
+    html = req.text
+    chapter_bs = BeautifulSoup(html, 'lxml')
+    chapters = chapter_bs.find('div', id='list')
+    chapters = chapters.find_all('a')
+    for chapter in tqdm(chapters):
+        chapter_name = chapter.string
+        url = server + chapter.get('href')
+        content = get_content(url)
+        with open(book_name, 'a', encoding='utf-8') as f:
+            f.write(chapter_name)
+            f.write('\n')
+            f.write('\n'.join(content))
+            f.write('\n')
