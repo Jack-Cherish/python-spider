9dbbffee2a480c0f83b7db8ff9b9c69b4fce796d
Jack-Cherish <c411184003@gmail.com>
2020-04-28 23:59:48 +0800

    dmzj

--- 2020/dmzj/cartoon.py
+++ 2020/dmzj/cartoon.py
@@ -45,30 +45,30 @@ for i, url in enumerate(tqdm(chapter_urls)):
     chapter_save_dir = os.path.join(save_dir, name)
     if name not in os.listdir(save_dir):
         os.mkdir(chapter_save_dir)
-        r = requests.get(url = url)
-        html = BeautifulSoup(r.text, 'lxml')
-        script_info = html.script
-        pics = re.findall('\d{13,14}', str(script_info))
-        for j, pic in enumerate(pics):
-            if len(pic) == 13:
-                pics[j] = pic + '0'
-        pics = sorted(pics, key=lambda x:int(x))
-        chapterpic_hou = re.findall('\|(\d{5})\|', str(script_info))[0]
-        chapterpic_qian = re.findall('\|(\d{4})\|', str(script_info))[0]
-        for idx, pic in enumerate(pics):
-            if pic[-1] == '0':
-                url = 'https://images.dmzj.com/img/chapterpic/' + chapterpic_qian + '/' + chapterpic_hou + '/' + pic[:-1] + '.jpg'
+    r = requests.get(url = url)
+    html = BeautifulSoup(r.text, 'lxml')
+    script_info = html.script
+    pics = re.findall('\d{13,14}', str(script_info))
+    for j, pic in enumerate(pics):
+        if len(pic) == 13:
+            pics[j] = pic + '0'
+    pics = sorted(pics, key=lambda x:int(x))
+    chapterpic_hou = re.findall('\|(\d{5})\|', str(script_info))[0]
+    chapterpic_qian = re.findall('\|(\d{4})\|', str(script_info))[0]
+    for idx, pic in enumerate(pics):
+        if pic[-1] == '0':
+            url = 'https://images.dmzj.com/img/chapterpic/' + chapterpic_qian + '/' + chapterpic_hou + '/' + pic[:-1] + '.jpg'
+        else:
+            url = 'https://images.dmzj.com/img/chapterpic/' + chapterpic_qian + '/' + chapterpic_hou + '/' + pic + '.jpg'
+        pic_name = '%03d.jpg' % (idx + 1)
+        pic_save_path = os.path.join(chapter_save_dir, pic_name)
+        with closing(requests.get(url, headers = download_header, stream = True)) as response:  
+            chunk_size = 1024  
+            content_size = int(response.headers['content-length'])  
+            if response.status_code == 200:
+                with open(pic_save_path, "wb") as file:  
+                    for data in response.iter_content(chunk_size=chunk_size):  
+                        file.write(data)  
             else:
-                url = 'https://images.dmzj.com/img/chapterpic/' + chapterpic_qian + '/' + chapterpic_hou + '/' + pic + '.jpg'
-            pic_name = '%03d.jpg' % (idx + 1)
-            pic_save_path = os.path.join(chapter_save_dir, pic_name)
-            with closing(requests.get(url, headers = download_header, stream = True)) as response:  
-                chunk_size = 1024  
-                content_size = int(response.headers['content-length'])  
-                if response.status_code == 200:
-                    with open(pic_save_path, "wb") as file:  
-                        for data in response.iter_content(chunk_size=chunk_size):  
-                            file.write(data)  
-                else:
-                    print('链接异常')
-        time.sleep(10)
+                print('链接异常')
+    time.sleep(10)
